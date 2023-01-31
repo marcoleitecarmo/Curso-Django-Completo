fab013d0c4b788a035ef11f86b7d8d9233174a24
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2023-01-31 10:38:07 -0300

    Testes com falhas a serem corrigidas

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,7 @@
-from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+from .test_recipe_base import Recipe, RecipeTestBase
+from unittest import skip
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -6,6 +9,53 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newuser'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+    @skip('Teste com erro no recipe = self.make_recipe_no_defautls()')
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.preparation_steps_is_html,
+            msg='Recipe preparation_steps_is_html is not False',
+        )
+
+    @skip('Teste com erro no recipe = self.make_recipe_no_defautls()')
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe is_published is not False',
+        )
+    
+    @skip('Erro de string vazia')
+    def test_recipe_string_representation(self):
+        self.recipe.title = 'Testing Representation'
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), 'Testing Representation')
