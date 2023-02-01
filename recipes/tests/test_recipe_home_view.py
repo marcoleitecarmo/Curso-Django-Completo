1b8dc01c56d5f7fb32daf50b75d00a69daf4567b
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2023-02-01 17:42:56 -0300

    divisão dos testes e  configuração de busca

---
+++ recipes/tests/test_recipe_home_view.py
@@ -0,0 +1,62 @@
+from django.urls import reverse, resolve
+from recipes import views
+from .test_recipe_base import RecipeTestBase
+from unittest import skip
+
+
+# Create your tests here.
+
+
+# Testes da Views
+class RecipeHomeViewTest(RecipeTestBase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_recipes_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
+        )
+
+    @skip('Teste com erro no self.client')
+    def test_recipes_home_template_loads_recipe(self):
+
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Check if one recipe exists
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
+        )
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 # from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.http.response import Http404
+from django.db.models import Q
 
 
 def home(request):
@@ -41,8 +42,16 @@ def search(request):
 
     if not search_term:
         raise Http404()
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{ search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
